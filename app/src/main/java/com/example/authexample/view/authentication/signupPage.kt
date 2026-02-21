7d95f7cd9ad12a45d9c99a049360a3495ac7b455
naruuu62 <nurilarifin270@gmail.com>
2026-02-21 22:04:12 +0700

    first commit

---
+++ app/src/main/java/com/example/authexample/view/authentication/signupPage.kt
@@ -0,0 +1,11 @@
+package com.example.authexample.view.authentication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+
+fun SignupPage(){
+
+}
